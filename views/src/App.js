bf930703ea9d7b7b4334830b48673a553afac6de
Geeku <ggongpei@gmail.com>
2018-02-02 12:29:26 +0800

    directory structure

---
+++ views/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        EEEmoticon
+      </main>
+    );
+  }
+}
+
+export default App;
